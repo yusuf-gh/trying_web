5b3c49a3013932df9c595d2fb187ee232aa4da90
yusuf-gh <snjar.meb@gmail.com>
2024-03-29 20:06:28 +0500

    ready for work

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
+class News(models.Model):
+    title = models.CharField(max_length=256, null=True, blank=True)
+    image = models.ImageField(upload_to='coffeeSort/', null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
 # Create your models here.
--- myapp/urls.py
+++ myapp/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('', views.main_page)
+   path('', views.main_page),
+   path('home', views.main_page),
+   path('news', views.news)
 
 ]
